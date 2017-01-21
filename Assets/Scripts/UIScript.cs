f1bfe9f77ca07318edb640dd84a9409d35de1334
ThatDudeMike <mgagne002@gmail.com>
2017-01-20 21:03:39 -0500

    Made UI Script and Started UI
    Made a UI Script and started the skeleton for the UI

---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,28 @@
+﻿//Mike's UI Script
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour
+{
+    public CanvasGroup storeGroup;
+    public CanvasGroup upgradeGroup;
+    public CanvasGroup itemGroup;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        storeGroup = GameObject.Find("StorePanel").gameObject.GetComponent<CanvasGroup>();
+        upgradeGroup = GameObject.Find("UpgradePanel").gameObject.GetComponent<CanvasGroup>();
+        itemGroup = GameObject.Find("ItemPanel").gameObject.GetComponent<CanvasGroup>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
