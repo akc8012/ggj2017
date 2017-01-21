5ebe264149e486567526c6ea7f069c1d24a29816
Andrew Colannino <akcolannino@email.neit.edu>
2017-01-21 08:58:24 -0500

    draggable item on pc and phone

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿// Andrew - draggable item class
+using UnityEngine;
 using System.Collections;
 
 public class Item : MonoBehaviour
@@ -11,6 +12,18 @@ public class Item : MonoBehaviour
 	
 	void Update()
 	{
-		
+		if (Input.touchCount != 0)
+		{
+			Vector3 touchPos = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, 10);
+			Vector3 screenToWorld = Camera.main.ScreenToWorldPoint(touchPos);
+			transform.position = screenToWorld;
+		}
+		else if (Input.GetMouseButton(0))
+		{
+			Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+			Vector3 screenToWorld = Camera.main.ScreenToWorldPoint(mousePos);
+			transform.position = screenToWorld;
+			print(screenToWorld);
+		}
 	}
 }
