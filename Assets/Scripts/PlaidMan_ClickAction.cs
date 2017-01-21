f0ec3e534682c03f9cdff01cc17d1a98e4d711ff
Tristan Sinclair <stillnight77@gmail.com>
2017-01-21 12:17:14 -0500

    Tristan Major Update #1
    Added a bunch of sprites including icons for Andrew that are separate
    from the buttons in the shop, finished drawing, positioning and
    animating the Canadian Man, "Plaid Man", and created a script to animate
    him on click.

---
+++ Assets/Scripts/PlaidMan_ClickAction.cs
@@ -0,0 +1,26 @@
+﻿// TRISTAN
+
+using UnityEngine;
+using System.Collections;
+
+// Placement:  Place on "Plaid Man" object in scene
+
+// Function:  Script animates Plaid Man and all children GameObjects on Mouse Click (no tap yet)
+
+public class PlaidMan_ClickAction : MonoBehaviour {
+
+	public GameObject[] children;
+
+	void Start () {
+
+	}
+
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			GetComponent<Animator> ().SetTrigger("ACTIVE");
+			for (int i = 0; i < this.children.Length; i++) {
+				children[i].GetComponent<Animator> ().SetTrigger("ACTIVE");
+			}
+		}
+	}
+}
