2750f057702a54023fb5f5181496391672418fb9
Andrew Colannino <akcolannino@email.neit.edu>
2017-01-21 09:25:37 -0500

    inputguy is optional

--- Assets/Scripts/Manager/Loader.cs
+++ Assets/Scripts/Manager/Loader.cs
@@ -8,13 +8,17 @@ public class Loader : MonoBehaviour
 	[SerializeField] GameObject scoreManager;
 	[SerializeField] GameObject itemManager;
 	[SerializeField] GameObject inputGuy;
+	[SerializeField] bool enableInputGuy = false;	// temporary
 
 	void Awake()
 	{
 		if (ScoreManager.instance == null)
 			Instantiate(scoreManager);
 
-		if (InputGuy.instance == null)
+		if (ItemManager.instance == null)
+			Instantiate(itemManager);
+
+		if (enableInputGuy && InputGuy.instance == null)
 			Instantiate(inputGuy);
 	}
 }
