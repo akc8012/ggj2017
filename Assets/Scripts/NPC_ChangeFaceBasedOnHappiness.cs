d5428437b70225713837efe5cf20a92491ade868
Tristan Sinclair <stillnight77@gmail.com>
2017-01-21 16:28:01 -0500

    Script to change faces
    asd

---
+++ Assets/Scripts/NPC_ChangeFaceBasedOnHappiness.cs
@@ -0,0 +1,42 @@
+﻿// TRISTAN
+
+using UnityEngine;
+using System.Collections;
+
+// Placement:  Place on the prefabs for ManSprite, WomanSprite and ChildSprite
+
+// Function:  Changes the "Face" child of the NPC that gets spawned based on NPCSkeleton happiness
+
+public class NPC_ChangeFaceBasedOnHappiness : MonoBehaviour {
+
+	// vars
+	NPCskeleton skeletonScript;
+
+	public GameObject face;
+
+	public Sprite[] spriteList;
+
+	void Start () {
+		skeletonScript = GetComponent<NPCskeleton>();
+	}
+
+	void Update () {
+		switch(skeletonScript.Happiness){
+		case 0:
+			face.GetComponent<SpriteRenderer> ().sprite = spriteList [0];
+			break;
+		case 1:
+			face.GetComponent<SpriteRenderer> ().sprite = spriteList [1];
+			break;
+		case 2:
+			face.GetComponent<SpriteRenderer> ().sprite = spriteList [2];
+			break;
+		case 3:
+			face.GetComponent<SpriteRenderer> ().sprite = spriteList [3];
+			break;
+		case 4:
+			face.GetComponent<SpriteRenderer> ().sprite = spriteList [4];
+			break;
+		}
+	}
+}
