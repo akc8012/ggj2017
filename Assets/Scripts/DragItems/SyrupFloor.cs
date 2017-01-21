67f5d6e14b796f025f26c887980122a4d59fb427
Andrew Colannino <akcolannino@email.neit.edu>
2017-01-21 16:52:20 -0500

    syrup splatter spawns on floor

---
+++ Assets/Scripts/DragItems/SyrupFloor.cs
@@ -0,0 +1,22 @@
+﻿// Andrew -- visual syrup for syrup bomb
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SyrupFloor : MonoBehaviour
+{
+	float yOffset = 0.25f;
+
+	void Start()
+	{
+		Vector3 floor = transform.position;
+		floor.y = GameObject.FindWithTag("Floor").transform.position.y;
+		floor.y += yOffset;
+		transform.position = floor;
+	}
+
+	void Update()
+	{
+		
+	}
+}
