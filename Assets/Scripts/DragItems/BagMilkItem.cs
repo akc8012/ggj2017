11b2fe2937277d124a3bcd257ff0534783b58828
Andrew Colannino <akcolannino@email.neit.edu>
2017-01-21 22:56:21 -0500

    bag milk item is finished
    rain of milk makes canada happy

--- Assets/Scripts/DragItems/BagMilkItem.cs
+++ Assets/Scripts/DragItems/BagMilkItem.cs
@@ -6,10 +6,15 @@ using UnityEngine;
 public class BagMilkItem : DragItem
 {
 	[SerializeField] GameObject rainParticles;
+	Collider col;
+	GameObject[] npcs;
 	float milkSprayingTime = 3;
+	float yOffset = 0.8f;
 
 	protected override void Init()
 	{
+		col = GetComponent<Collider>();
+		npcs = GameObject.FindGameObjectsWithTag("NPC");
 		rainParticles = (GameObject)Instantiate(rainParticles, transform.position, rainParticles.transform.rotation);
 		canBeDropped = false;
 		StartCoroutine(WhenToStopSprayingMilk());
@@ -17,8 +22,23 @@ public class BagMilkItem : DragItem
 
 	protected override void PerformContinuousAction()
 	{
-		rainParticles.transform.position = transform.position;
-		//print("I am spraying milk");
+		Vector3 rainPos = transform.position;
+		rainPos.y -= yOffset;
+		rainParticles.transform.position = rainPos;
+		
+		for (int i = 0; i < npcs.Length; i++)
+		{
+			if (npcs[i] != null)    // could become null if they die
+			{
+				Vector3 npcPos = Camera.main.WorldToScreenPoint(npcs[i].transform.position);
+				Vector3 min = Camera.main.WorldToScreenPoint(col.bounds.min);
+				Vector3 max = Camera.main.WorldToScreenPoint(col.bounds.max);
+				if (npcPos.x >= min.x && npcPos.x <= max.x)
+				{
+					npcs[i].GetComponent<NPCskeleton>().MakeHappy();
+				}
+			}
+		}
 	}
 
 	IEnumerator WhenToStopSprayingMilk()
