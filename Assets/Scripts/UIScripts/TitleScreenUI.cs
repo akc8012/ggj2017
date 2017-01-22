854cea151aaedc21328f096f89b454dfc312b27a
Andrew Colannino <akcolannino@email.neit.edu>
2017-01-22 12:16:38 -0500

    title screen hooked up

---
+++ Assets/Scripts/UIScripts/TitleScreenUI.cs
@@ -0,0 +1,23 @@
+﻿// Andrew
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenUI : MonoBehaviour
+{
+	[SerializeField] Button playButt;
+
+	void Start()
+	{
+		playButt.onClick.AddListener(PlayGame);
+		print("jere");
+	}
+
+	void PlayGame()
+	{
+		print("click");
+		SceneManager.LoadScene("TristanScene (Duplicate)", LoadSceneMode.Single);
+	}
+}
