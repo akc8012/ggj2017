718a34494e6f5b8f78fe856c3add788472f43b48
ThatDudeMike <mgagne002@gmail.com>
2017-01-22 11:02:14 -0500

    Fixed Major UI Bugs
    Major bugs.

--- Assets/Scripts/UIScripts/ItemButtonScript.cs
+++ Assets/Scripts/UIScripts/ItemButtonScript.cs
@@ -11,6 +11,8 @@ public class ItemButtonScript : MonoBehaviour
     int price;
     [SerializeField]
     string itemName;
+    [SerializeField]
+    string displayName;
 
     Button parentButton;
     InventoryBarScript invScript;
@@ -59,7 +61,7 @@ public class ItemButtonScript : MonoBehaviour
                 case false:
                     ScoreManager.instance.AddScore(-price);
                     invScript.SendMessage("AddItem", itemName);
-                    store.DisplayFeedback(itemName);
+                    store.DisplayFeedback(displayName);
                     break;
             }
         }
